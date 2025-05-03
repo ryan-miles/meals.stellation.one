f314b9282e96a4df02c6304e7e264df64a522591
Ryan <rlmiles77@gmail.com>
2025-05-03 17:55:43 -0400

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,5 @@
+const API_CONFIG = {
+    api_endpoint: "https://zw2vq81phe.execute-api.us-east-1.amazonaws.com/generate-recipe",
+    gateway_id: "zw2vq81phe",
+    lambda_name: "meals-gemini-generator"
+};
