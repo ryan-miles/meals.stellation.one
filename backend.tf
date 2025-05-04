704fb02cdbef8fcf5ddf8b510b96bf1702bd45fe
Ryan <rlmiles77@gmail.com>
2025-05-04 13:19:00 -0400

    Removed old tf files.

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "meals-stellation-terraform-state"
-    key    = "terraform.tfstate"
-    region = "us-east-1"  // Change this to your AWS region if different
-  }
-}
